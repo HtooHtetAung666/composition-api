562a5d8a1d5a45fc30cc1b186bac66ce1968a45f
ryzen666 <htoohtetaung26@gmail.com>
2022-08-25 00:46:59 +0630

    single post component, create page, post backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,39 @@
 <template>
   <div class="home">
-    <PostList :posts="posts"></PostList>
+    <div v-if="posts.length>0">
+      <PostList :posts="posts"></PostList>
+    </div>
+    <div v-else>
+      <Spinner></Spinner>
+    </div>
   </div>
 </template>
 
 <script>
+import Spinner from '../components/Spinner'
 import PostList from '../components/PostList'
-import { ref } from 'vue'
+import getPosts from '../composables/getPosts'
 
 export default {
-  components: { PostList },  
+  components: {
+    Spinner, PostList },  
 
   setup() {
-    let posts = ref([
-      {title: 'post 1', body: 'lorem ipsum 1'},
-      {title: 'post 2', body: 'lorem ipsum 2'}
-    ])
 
-    return { posts }
+    let { posts, error, load } = getPosts();
+
+    load();
+    
+    return { posts, error};
+
   }
 }
 </script>
+
+<style>
+  .home {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+</style>
